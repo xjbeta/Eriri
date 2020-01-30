03df4d7280376fb52a815e00db95f8607d752289
xjbeta <xjbeta.tiny@gmail.com>
2020-01-30 15:14:10 +0800

    Update supported extensions.

--- Eriri/Utils/Utils.swift
+++ Eriri/Utils/Utils.swift
@@ -16,8 +16,15 @@ class Utils: NSObject {
     fileprivate override init() {
     }
     
-    let subtitleTypes = ["cdg", "idx", "srt", "sub", "utf", "ass", "ssa", "aqt", "jss", "psb", "rt", "smi", "txt", "smil", "stl", "usf", "dks", "pjs", "mpl2", "mks", "vtt", "ttml", "dfxp"]
-    let videoTypes = ["ts", "webm", "ogg", "ogm", "mp4", "mov", "ps", "mpjpeg", "wav", "flv", "mpeg1", "mkv", "raw", "avi", "asf", "wmv"]
+    let supportedFileExtensions = ["3g2", "3gp", "3gp2", "3gpp", "amv", "asf", "avi", "bik", "bin", "crf", "divx", "drc", "dv", "evo", "f4v", "flv", "gvi", "gxf", "iso", "m1v", "m2v", "m2t", "m2ts", "m4v", "mkv", "mov", "mp2", "mp2v", "mp4", "mp4v", "mpe", "mpeg", "mpeg1", "mpeg2", "mpeg4", "mpg", "mpv2", "mts", "mtv", "mxf", "mxg", "nsv", "nuv", "ogg", "ogm", "ogv", "ogx", "ps", "rec", "rm", "rmvb", "rpl", "thp", "tod", "ts", "tts", "txd", "vlc", "vob", "vro", "webm", "wm", "wmv", "wtv", "xesc"]
+    
+    let supportedSubtitleFileExtensions = ["cdg", "idx", "srt", "sub", "utf", "ass", "ssa", "aqt", "jss", "psb", "rt", "smi", "txt", "smil", "stl", "usf", "dks", "pjs", "mpl2", "mks", "vtt", "ttml", "dfxp"]
+    
+    let supportedAudioFileExtensions = ["3ga", "669", "a52", "aac", "ac3", "adt", "adts", "aif", "aifc", "aiff", "amb", "amr", "aob", "ape", "au", "awb", "caf", "dts", "flac", "it", "kar", "m4a", "m4b", "m4p", "m5p", "mid", "mka", "mlp", "mod", "mpa", "mp1", "mp2", "mp3", "mpc", "mpga", "mus", "oga", "ogg", "oma", "opus", "qcp", "ra", "rmi", "s3m", "sid", "spx", "tak", "thd", "tta", "voc", "vqf", "w64", "wav", "wma", "wv", "xa", "xm"]
+    
+    let supportedPlaylistFileExtensions = ["asx", "b4s", "cue", "ifo", "m3u", "m3u8", "pls", "ram", "rar", "sdp", "vlc", "xspf", "wax", "wvx", "zip", "conf"]
+    
+    let supportedProtocolSchemes = ["rtsp", "mms", "mmsh", "udp", "rtp", "rtmp", "sftp", "ftp", "smb"]
     
     lazy var mediaOpenPanel: NSOpenPanel = {
         let p = NSOpenPanel()
@@ -25,7 +32,7 @@ class Utils: NSObject {
         p.canChooseDirectories = false
         p.canDownloadUbiquitousContents = false
         p.canResolveUbiquitousConflicts = false
-        p.allowedFileTypes = videoTypes
+        p.allowedFileTypes = supportedFileExtensions
         return p
     }()
     
