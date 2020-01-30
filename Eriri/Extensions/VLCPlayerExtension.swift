55a22d51c0cad2aec0bf4c74a396b7381a5420e0
xjbeta <xjbeta.tiny@gmail.com>
2020-01-30 15:07:03 +0800

    VLCMediaPlayer metaData().

--- Eriri/Extensions/VLCPlayerExtension.swift
+++ Eriri/Extensions/VLCPlayerExtension.swift
@@ -17,4 +17,52 @@ extension VLCMediaPlayer {
             play()
         }
     }
+    
+    func metaData() -> [(String, String)] {
+        guard let media = media else { return [] }
+        let metaDictionaryKeys =
+//            [VLCMetaInformationTitle,
+//             VLCMetaInformationArtist,
+//             VLCMetaInformationGenre,
+//             VLCMetaInformationCopyright,
+//             VLCMetaInformationAlbum,
+//             VLCMetaInformationTrackNumber,
+//             VLCMetaInformationDescription,
+//             VLCMetaInformationRating,
+//             VLCMetaInformationDate,
+//             VLCMetaInformationSetting,
+//             VLCMetaInformationURL,
+//             VLCMetaInformationLanguage,
+//             VLCMetaInformationNowPlaying,
+//             VLCMetaInformationPublisher,
+//             VLCMetaInformationEncodedBy,
+//             VLCMetaInformationArtworkURL,
+//             VLCMetaInformationArtwork,
+//             VLCMetaInformationTrackID,
+//             VLCMetaInformationTrackTotal,
+//             VLCMetaInformationDirector,
+//             VLCMetaInformationSeason,
+//             VLCMetaInformationEpisode,
+//             VLCMetaInformationShowName,
+//             VLCMetaInformationActors,
+//             VLCMetaInformationAlbumArtist,
+//             VLCMetaInformationDiscNumber]
+        
+        [VLCMetaInformationTitle,
+        VLCMetaInformationArtist,
+        VLCMetaInformationAlbum,
+        VLCMetaInformationDate,
+        VLCMetaInformationGenre,
+        VLCMetaInformationTrackNumber,
+        VLCMetaInformationDiscNumber,
+        VLCMetaInformationNowPlaying,
+        VLCMetaInformationLanguage]
+        
+        var re = [(String, String)]()
+        metaDictionaryKeys.forEach {
+            let data = media.metadata(forKey: $0)
+            re.append(($0, data))
+        }
+        return re
+    }
 }
