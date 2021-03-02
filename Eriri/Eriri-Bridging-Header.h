e7f156579a4aef47f42a5bc4c8dd16d71c6b4d3d
xjbeta <xjbeta.tiny@gmail.com>
2021-03-02 12:22:29 +0800

    Bridging Header.

--- Eriri/Eriri-Bridging-Header.h
+++ Eriri/Eriri-Bridging-Header.h
@@ -2,8 +2,13 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <vlc/vlc.h>
+#import <vlc/libvlc.h>
 #import <vlc/libvlc_internal.h>
 #import <vlc/media_player_internal.h>
-#import <vlc/player.h>
 
+
+
+#import <ass.h>
+#import <ass_types.h>
+
+#import "LibassBlendImage.h"
